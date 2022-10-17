7fc07eea043abf1961189a7506df36be13c66b64
Alyson Rodrigo Gustavo da Silva <g.dasilva@hotmail.com>
2022-10-17 18:39:39 -0300

    Ajuste final KJSencha

--- src/Service/Factory/ApiFactory.php
+++ src/Service/Factory/ApiFactory.php
@@ -2,15 +2,10 @@
 
 namespace KJSencha\Service\Factory;
 
-use Interop\Container\ContainerInterface;
-use KJSencha\Direct\Remoting\Api\Api;
-use KJSencha\Direct\Remoting\Api\Factory\ApiBuilder;
-use Laminas\Cache\Storage\StorageInterface;
-use Laminas\Config\Config;
-use Laminas\Http\PhpEnvironment\Request;
 use Laminas\Router\Http\RouteInterface;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Laminas\ServiceManager\ServiceLocatorInterface;
+use Psr\Container\ContainerInterface;
 
 class ApiFactory implements FactoryInterface
 {
@@ -20,22 +15,22 @@ class ApiFactory implements FactoryInterface
      *
      * @return \KJSencha\Direct\Remoting\Api\Api
      */
-    public function createService(ServiceLocatorInterface $serviceLocator)
+    public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
     {
         /* @var $config array */
-        $config = $serviceLocator->get('Config');
+        $config = $container->get('Config');
         /* @var $cache \Laminas\Cache\Storage\StorageInterface */
-        $cache = $serviceLocator->get('kjsencha.cache');
+        $cache = $container->get('kjsencha.cache');
         /* @var $router RouteInterface */
-        $router = $serviceLocator->get('HttpRouter');
+        $router = $container->get('HttpRouter');
         /* @var $api \KJSencha\Direct\Remoting\Api\Api */
         $api = $cache->getItem($config['kjsencha']['cache_key'], $success);
 
         if (!$success) {
             /* @var $apiFactory \KJSencha\Direct\Remoting\Api\Factory\ApiBuilder */
-            $apiFactory = $serviceLocator->get('kjsencha.apibuilder');
+            $apiFactory = $container->get('kjsencha.apibuilder');
             /* @var $request \Zend\Http\PhpEnvironment\Request */
-            $request = $serviceLocator->get('Request');
+            $request = $container->get('Request');
             $api = $apiFactory->buildApi($config['kjsencha']['direct']);
             $url = $request->getBasePath() . $router->assemble(
                     array('action' => 'rpc'),
